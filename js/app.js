ae21ecb1cdc646aac2587cedabe76f206142ade3
paulajuliannamartin-ux <paula.julianna.martin@gmail.com>
2025-10-01 10:15:08 +0200

    Start working in app.js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+console.log("Hello World!");
+console.log("This is line 1");
+console.log("This is line 2");
+console.log("This is line 3"); console.log("This is line 4");
+console.log("This is line 5");
